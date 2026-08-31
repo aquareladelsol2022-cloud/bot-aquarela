0905c4e4d1551379a533a58f05804872f2911c2e
aquareladelsol2022-cloud <aquareladelsol2022@gmail.com>
2026-08-30 22:05:12 -0500

    Add files via upload

--- src/services/whatsapp.service.ts
+++ src/services/whatsapp.service.ts
@@ -6,6 +6,10 @@ import os from 'os';
 
 export const whatsappClient = new Client({
     authStrategy: new LocalAuth(),
+    webVersionCache: {
+        type: 'remote',
+        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+    },
     puppeteer: {
         executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
         args: [
